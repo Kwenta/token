a174e7ef814c64ce554e8866685a733de8c1280c
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-11-09 18:51:12 -0500

    feat: switched to address resolver for getting synthetix contracts

--- contracts/ExchangerProxy.sol
+++ contracts/ExchangerProxy.sol
@@ -1,17 +1,32 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import "./interfaces/IAddressResolver.sol";
 import "./interfaces/ISynthetix.sol";
 import "./interfaces/IExchanger.sol";
 
 contract ExchangerProxy {
 
-    ISynthetix synthetix;
-    IExchanger exchanger;
+    IAddressResolver addressResolver;
+    bytes32 private constant CONTRACT_SYNTHETIX = "Synthetix";
+    bytes32 private constant CONTRACT_EXCHANGER = "Exchanger";
 
-    constructor(address _synthetixProxy, address _exchanger) {
-        synthetix = ISynthetix(_synthetixProxy);
-        exchanger = IExchanger(_exchanger);
+    constructor(address _addressResolver) {
+        addressResolver = IAddressResolver(_addressResolver);
+    }
+
+    function synthetix() internal view returns (ISynthetix) {
+        return ISynthetix(addressResolver.requireAndGetAddress(
+            CONTRACT_SYNTHETIX, 
+            "Could not get Synthetix"
+        ));
+    }
+
+    function exchanger() internal view returns (IExchanger) {
+        return IExchanger(addressResolver.requireAndGetAddress(
+            CONTRACT_EXCHANGER, 
+            "Could not get Exchanger"
+        ));
     }
     
     function exchangeWithTraderScoreTracking(
@@ -22,10 +37,10 @@ contract ExchangerProxy {
         bytes32 trackingCode
     ) external returns (uint amountReceived) {
         // Get fee
-        uint fee = exchanger.feeRateForExchange(sourceCurrencyKey, destinationCurrencyKey);
+        uint fee = exchanger().feeRateForExchange(sourceCurrencyKey, destinationCurrencyKey);
 
         // Execute typical exchange
-        uint received = synthetix.exchangeWithTracking(
+        uint received = synthetix().exchangeWithTracking(
             sourceCurrencyKey, 
             sourceAmount, 
             destinationCurrencyKey, 
