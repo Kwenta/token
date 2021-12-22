1a2ed2d3babc82965a317190f0ec61006cc6028b
JChiaramonte7 <jeremychiaramonte@gmail.com>
2021-12-22 10:05:56 -0500

    feat: port over V2 and stripped down

---
+++ contracts/interfaces/IRewardEscrowV2.sol
@@ -0,0 +1,58 @@
+pragma solidity >=0.4.24;
+pragma experimental ABIEncoderV2;
+
+library VestingEntries {
+    struct VestingEntry {
+        uint64 endTime;
+        uint256 escrowAmount;
+    }
+    struct VestingEntryWithID {
+        uint64 endTime;
+        uint256 escrowAmount;
+        uint256 entryID;
+    }
+}
+
+interface IRewardEscrowV2 {
+    // Views
+    function balanceOf(address account) external view returns (uint);
+
+    function numVestingEntries(address account) external view returns (uint);
+
+    function totalEscrowedAccountBalance(address account) external view returns (uint);
+
+    function totalVestedAccountBalance(address account) external view returns (uint);
+
+    function getVestingQuantity(address account, uint256[] calldata entryIDs) external view returns (uint);
+
+    function getVestingSchedules(
+        address account,
+        uint256 index,
+        uint256 pageSize
+    ) external view returns (VestingEntries.VestingEntryWithID[] memory);
+
+    function getAccountVestingEntryIDs(
+        address account,
+        uint256 index,
+        uint256 pageSize
+    ) external view returns (uint256[] memory);
+
+    function getVestingEntryClaimable(address account, uint256 entryID) external view returns (uint);
+
+    function getVestingEntry(address account, uint256 entryID) external view returns (uint64, uint256);
+
+    // Mutative functions
+    function vest(uint256[] calldata entryIDs) external;
+
+    function createEscrowEntry(
+        address beneficiary,
+        uint256 deposit,
+        uint256 duration
+    ) external;
+
+    function appendVestingEntry(
+        address account,
+        uint256 quantity,
+        uint256 duration
+    ) external;
+}
