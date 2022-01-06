3eb760ffe323eaa13831339bef022ea1cf6f1361
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-01-06 17:40:48 -0500

    fix: mint 1st week test with treasury diversion

--- test/integration/mint.behavior.ts
+++ test/integration/mint.behavior.ts
@@ -7,6 +7,7 @@ describe("Mint", () => {
     const SYMBOL = "KWENTA";
     const INITIAL_SUPPLY = ethers.utils.parseUnits("313373");
     const TREASURY_DAO_ADDRESS = "0x0000000000000000000000000000000000000001";
+    const INFLATION_DIVERSION_BPS = 2000;
 
     let safeDecimalMath,
         supplySchedule: Contract,
@@ -43,9 +44,11 @@ describe("Mint", () => {
             NAME,
             SYMBOL,
             INITIAL_SUPPLY,
+            owner.address,
             TREASURY_DAO_ADDRESS,
             mockStakingRewards.address,
-            supplySchedule.address
+            supplySchedule.address,
+            INFLATION_DIVERSION_BPS
         );
         await kwenta.deployed();
 
@@ -56,22 +59,29 @@ describe("Mint", () => {
 
     it("No inflationary supply to mint", async () => {
         await expect(kwenta.mint()).to.be.revertedWith("No supply is mintable");
-        expect(
-            await kwenta.balanceOf(mockStakingRewards.address)
-        ).to.equal(0);
+        expect(await kwenta.balanceOf(mockStakingRewards.address)).to.equal(0);
     });
 
     it("Mint inflationary supply 1 week later", async () => {
         const [owner] = await ethers.getSigners();
         const MINTER_REWARD = ethers.utils.parseUnits("200");
+        const FIRST_WEEK_MINT = INITIAL_SUPPLY.mul(60)
+            .div(100)
+            .div(52)
+            .sub(MINTER_REWARD);
+
+        // We subtract treasury inflationary diversion amount so that stakers get remainder (after truncation)
+        const FIRST_WEEK_STAKING_REWARDS = FIRST_WEEK_MINT.sub(
+            FIRST_WEEK_MINT.mul(INFLATION_DIVERSION_BPS).div(10000)
+        );
 
         expect(await kwenta.balanceOf(owner.address)).to.equal(0);
         await network.provider.send("evm_increaseTime", [604800]);
         await kwenta.mint();
 
         // Make sure this is equivalent to first week distribution
-        expect(
-            await kwenta.balanceOf(mockStakingRewards.address)
-        ).to.equal(INITIAL_SUPPLY.mul(60).div(100).div(52).sub(MINTER_REWARD));
+        expect(await kwenta.balanceOf(mockStakingRewards.address)).to.equal(
+            FIRST_WEEK_STAKING_REWARDS
+        );
     });
 });
