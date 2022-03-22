c4e6c5b6694c77cd3b54bb73b29abfb932b9514c
Jared Borders <jlbxsxs@gmail.com>
2022-03-22 17:27:44 -0500

    Add canExchangeFor to check approval status

--- contracts/interfaces/IDelegateApprovals.sol
+++ contracts/interfaces/IDelegateApprovals.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.4.24;
 
 // https://docs.synthetix.io/contracts/source/interfaces/idelegateapprovals
 interface IDelegateApprovals {
+    // Views
+    function canExchangeFor(address authoriser, address delegate) external view returns (bool);
     // Mutative
     function approveExchangeOnBehalf(address delegate) external;
     function removeExchangeOnBehalf(address delegate) external;
