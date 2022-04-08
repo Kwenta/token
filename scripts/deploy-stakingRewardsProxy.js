bd0a97de98d07999ccc185ce22ca5b0b552609fc
Jared Borders <jlbxsxs@gmail.com>
2022-04-07 19:56:01 -0500

    Update deploy script

--- scripts/deploy-stakingRewardsProxy.js
+++ scripts/deploy-stakingRewardsProxy.js
@@ -4,7 +4,6 @@ async function main() {
 
   owner = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
   rewardsDistribution = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
-  rewardsTokenAddress = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc";
   stakingTokenAddress = "0x90f79bf6eb2c4f870365e785982e1f101e93b906";
   rewardsEscrowAddress = "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65";
   nDaysStartRewards = 3;
@@ -38,7 +37,6 @@ async function main() {
         [
         owner, 
         rewardsDistribution, 
-        rewardsTokenAddress, 
         stakingTokenAddress, 
         rewardsEscrowAddress,
         nDaysStartRewards
