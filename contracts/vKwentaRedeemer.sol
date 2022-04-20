0a0e6c6e24c36d59323de89c6a4c6669f154868e
Jared Borders <jlbxsxs@gmail.com>
2022-04-20 16:03:32 -0500

    Update comments

--- contracts/vKwentaRedeemer.sol
+++ contracts/vKwentaRedeemer.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import './interfaces/IvKwentaRedeemer.sol';
 import './utils/ERC20.sol';
 
+/// @title A redemption contract for Kwenta
+/// @dev All vKwenta used for redemption is locked within this contract
 contract vKwentaRedeemer is IvKwentaRedeemer {
-    // token to be burned
+    /// token to be burned
     address public immutable vToken;
-    // token to be redeemed
+    /// token to be redeemed
     address public immutable token;
-
 
     event Redeemed(address redeemer, uint256 redeemedAmount);
 
@@ -19,27 +19,25 @@ contract vKwentaRedeemer is IvKwentaRedeemer {
         token = _token;
     }
 
-    /*
-     * Allows caller to redeem an equivalent amount of token for vToken
-     * @notice caller must approve this contract to spend vToken
-     * @notice vToken is burned prior to transfer of token
-     */
+    /// Allows caller to redeem an equivalent amount of token for vToken
+    /// @dev caller must approve this contract to spend vToken
+    /// @notice vToken is locked within this contract prior to transfer of token
     function redeem() override external {
         uint vTokenBalance = IERC20(vToken).balanceOf(msg.sender);
 
-        // ensure valid balance
+        /// ensure valid balance
         require(vTokenBalance > 0, "vKwentaRedeemer: No balance to redeem");
         require(vTokenBalance <= IERC20(token).balanceOf(address(this)), 
             "vKwentaRedeemer: Insufficient contract balance"
         );
 
-        // lock vToken in this contract
+        /// lock vToken in this contract
         require(
             IERC20(vToken).transferFrom(msg.sender, address(this), vTokenBalance),
             "vKwentaRedeemer: vToken transfer failed"
         );
 
-        // transfer token
+        /// transfer token
         require(
             IERC20(token).transfer(msg.sender, vTokenBalance),
             "vKwentaRedeemer: token transfer failed"
@@ -47,5 +45,4 @@ contract vKwentaRedeemer is IvKwentaRedeemer {
 
         emit Redeemed(msg.sender, vTokenBalance);
     }
-
 }
