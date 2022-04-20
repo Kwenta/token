56f2827e1b1f049df899e72a028fbd16b556f0bc
Jared Borders <jlbxsxs@gmail.com>
2022-04-20 16:30:22 -0500

    refactor test

--- test/local/contracts/ControlL2MerkleDistributor.test.ts
+++ test/local/contracts/ControlL2MerkleDistributor.test.ts
@@ -12,6 +12,31 @@ require('chai')
     .use(smock.matchers)
     .should();
 
+// reproduces abi.encodeWithSignature()
+function generateMessage(
+    index: number,
+    destAccount: string,
+    amount: number,
+    merkleProof: string[]
+) {
+    const abiCoder = ethers.utils.defaultAbiCoder;
+    const sig = ethers.utils
+        .keccak256(
+            ethers.utils.toUtf8Bytes(
+                'claimToAddress(uint256,address,uint256,bytes32[])'
+            )
+        )
+        .substring(0, 10)
+        .concat('00000000000000000000000000000000'); // 32 bytes
+    const messageWithoutSig = abiCoder
+        .encode(
+            ['uint256', 'address', 'uint256', 'bytes32[]'],
+            [index, destAccount, amount, merkleProof]
+        )
+        .substring(34);
+    return sig.concat(messageWithoutSig);
+}
+
 describe('Control L2 MerkleDistributor from L1', function () {
     let controlL2MerkleDistributor: Contract;
     let crossDomainMessenger: FakeContract;
@@ -19,8 +44,6 @@ describe('Control L2 MerkleDistributor from L1', function () {
     let user1: SignerWithAddress;
     const MerkleDistributorL2Address =
         '0x0000000000000000000000000000000000000000';
-
-    const abiCoder = ethers.utils.defaultAbiCoder;
 
     beforeEach(async () => {
         [owner, user1] = await ethers.getSigners();
@@ -47,32 +70,17 @@ describe('Control L2 MerkleDistributor from L1', function () {
             '0x6430c7c7042d2c643dd7db25384f984592d856412f1022a575fe2ea01b2ac59e',
         ];
 
+        const message = generateMessage(index, destAccount, amount, merkleProof);
+
         await controlL2MerkleDistributor.claimToAddress(
             index,
             destAccount,
             amount,
             merkleProof
         );
-
-        const sig = ethers.utils
-            .keccak256(
-                ethers.utils.toUtf8Bytes(
-                    'claimToAddress(uint256,address,uint256,bytes32[])'
-                )
-            )
-            .substring(0, 10)
-            .concat('00000000000000000000000000000000'); // 32 bytes
-        const messageWithoutSig = abiCoder
-            .encode(
-                ['uint256', 'address', 'uint256', 'bytes32[]'],
-                [index, destAccount, amount, merkleProof]
-            )
-            .substring(34);
-        const messageWithSig = sig.concat(messageWithoutSig);
-
         expect(crossDomainMessenger.sendMessage).to.have.been.calledWith(
             MerkleDistributorL2Address,
-            messageWithSig,
+            message,
             1000000
         );
     });
