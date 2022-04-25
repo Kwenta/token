93fb6783114eeee888bd46eb7d94471e019e491f
JaredBorders <44350516+JaredBorders@users.noreply.github.com>
2022-04-25 10:10:19 -0500

    Multisig contract claim from L1 on L2 (#104)
    * update interface
    
    * initial claimToAddress draft
    
    * Add eth-optimism contracts and implement claimToAddress
    
    * Implement L1 contract and interface for messaging
    
    * Add test for L1 call to crossDomainMessenger
    
    * refactor test
    
    * Update contract to set crossDomainMessengerAddr address on deployment
    
    * Add test for claimToAddress and update distributor deployment params
    
    * fix bug
    
    * Removed immutable state variable
    
    * Update tests

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -6,12 +6,19 @@ import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./interfaces/IRewardEscrow.sol";
 import "./interfaces/IMerkleDistributor.sol";
 
+import { ICrossDomainMessenger } from 
+    "@eth-optimism/contracts/libraries/bridge/ICrossDomainMessenger.sol";
+
 contract MerkleDistributor is IMerkleDistributor {
     // escrow for tokens claimed
     address public immutable override rewardEscrow;
-    
+
     address public immutable override token;
     bytes32 public immutable override merkleRoot;
+
+    // communication between L1 and L2 is enabled by two special smart contracts called the "messengers"
+    // and below is the address for the messenger on L2
+    address private crossDomainMessengerAddr = 0x4200000000000000000000000000000000000007;
 
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
@@ -49,5 +56,27 @@ contract MerkleDistributor is IMerkleDistributor {
         IRewardEscrow(rewardEscrow).createEscrowEntry(account, amount, 52 weeks);
         
         emit Claimed(index, account, amount);
+    }
+
+    function claimToAddress(uint256 index, address destAccount, uint256 amount, bytes32[] calldata merkleProof) external override {
+        require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
+        require(msg.sender == crossDomainMessengerAddr, 
+            "MerkleDistributor: Only the OVM-ICrossDomainMessenger can call this function"
+        );
+
+        // caller address from L1 (effectively the msg.sender on L1)
+        address caller = ICrossDomainMessenger(crossDomainMessengerAddr).xDomainMessageSender();
+
+        // Verify the merkle proof with the L1 caller's address
+        bytes32 node = keccak256(abi.encodePacked(index, caller, amount));
+        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+
+        // Mark it claimed and send the token to RewardEscrow
+        _setClaimed(index);
+        IERC20(token).approve(rewardEscrow, amount);
+        // @notice destAccount is NOT necessarily the caller's address
+        IRewardEscrow(rewardEscrow).createEscrowEntry(destAccount, amount, 52 weeks);
+        
+        emit Claimed(index, caller, amount);
     }
 }
