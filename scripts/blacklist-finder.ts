1ed1a34c22ad316eca73bf11df82f1bc7c5be261
Jared Borders <jlbxsxs@gmail.com>
2022-06-14 16:44:20 -0500

    refactor and ensure synthetix treasury is not in exploited list

--- scripts/blacklist-finder.ts
+++ scripts/blacklist-finder.ts
@@ -15,22 +15,9 @@ async function main() {
         stakerDistribution,
         traderDistribution
     );
-    
-    // used for logging (later)
-    let x = 0;
-    let y = 0;
-    let z = 0;
 
-    // hash distributions for quick look-up
-    let distributionsMap = new Map<string, string>();
-
-    distributions.forEach(({ address }) => {
-        // will throw if invalid address
-        ethers.utils.getAddress(address);
-
-        distributionsMap.set(address, address);
-        x++;
-    });
+    let savedWalletCount = 0;
+    let exploitedWalletCount = 0;
 
     // hash savedWallets for quick look-up
     let savedWalletsMap = new Map<string, string>();
@@ -40,8 +27,12 @@ async function main() {
         // will throw if invalid address
         ethers.utils.getAddress(address);
 
+        if (savedWalletsMap.has(address)) {
+            console.log("savedWalletsMap REPEAT");
+        }
+
         savedWalletsMap.set(address, address);
-        y++;
+        savedWalletCount++;
     });
     savedWallets2.forEach(({ data }) => {
         let address = data.multisig.slice(49, 91);
@@ -49,8 +40,12 @@ async function main() {
         // will throw if invalid address
         ethers.utils.getAddress(address);
 
+        if (savedWalletsMap.has(address)) {
+            console.log("savedWalletsMap REPEAT");
+        }
+
         savedWalletsMap.set(address, address);
-        y++;
+        savedWalletCount++;
     });
 
     // hash exploitedWallets for quick look-up
@@ -61,8 +56,12 @@ async function main() {
         // will throw if invalid address
         ethers.utils.getAddress(address);
 
+        if (exploitedWalletsMap.has(address)) {
+            console.log("exploitedWalletsMap REPEAT");
+        }
+
         exploitedWalletsMap.set(address, address);
-        z++;
+        exploitedWalletCount++;
     });
 
     // determine whitelisted and blacklisted multisig addresses
@@ -71,6 +70,9 @@ async function main() {
     let unaccounted: string[] = [];
 
     distributions.forEach(({ address }) => {
+        // will throw if invalid address
+        ethers.utils.getAddress(address);
+
         if (exploitedWalletsMap.has(address)) {
             blacklisted.push(address);
         } else if (savedWalletsMap.has(address)) {
@@ -80,21 +82,24 @@ async function main() {
         }
     });
 
-    // log total number of *eligible for KWENTA* addresses that are:
-    // (1) whitelisted
-    // (2) blacklisted
-    // (3) neither
-    console.log('\n# of whitelisted: ' + whitelisted.length);
-    console.log('# of blacklisted: ' + blacklisted.length);
-    console.log('# of unaccounted: ' + unaccounted.length);
-
     // log total number of addresses that are:
     // (1) eligible for KWENTA
     // (2) saved by OE
     // (3) exploited by hack
-    console.log('\ntotal distributions: ' + x);
-    console.log('total savedWallets: ' + y);
-    console.log('total exploitedWallets: ' + z + '\n');
+    console.log('\n');
+    console.log('# of distributions: ' + distributions.length);
+    console.log('# of savedWallets: ' + savedWalletCount);
+    console.log('# of exploitedWallets: ' + exploitedWalletCount);
+    console.log('\n');
+
+    // log total number of *eligible for KWENTA* addresses that are:
+    // (1) whitelisted
+    // (2) blacklisted
+    // (3) neither
+    console.log('# of distributions that are whitelisted: ' + whitelisted.length);
+    console.log('# of distributions that are blacklisted: ' + blacklisted.length);
+    console.log('# of distributions that are unaccounted for: ' + unaccounted.length);
+    console.log('\n');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
