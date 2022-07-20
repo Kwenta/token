56869802f84699841847c7abe0ed595ad4ad9e6c
Jared Borders <jlbxsxs@gmail.com>
2022-07-20 14:00:53 -0500

    ✨ Prettify

--- contracts/vKwentaRedeemer.sol
+++ contracts/vKwentaRedeemer.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './interfaces/IvKwentaRedeemer.sol';
-import './utils/ERC20.sol';
+import "./interfaces/IvKwentaRedeemer.sol";
+import "./utils/ERC20.sol";
 
 /// @title A redemption contract for Kwenta
 /// @dev All vKwenta used for redemption is locked within this contract
@@ -22,18 +22,23 @@ contract vKwentaRedeemer is IvKwentaRedeemer {
     /// Allows caller to redeem an equivalent amount of token for vToken
     /// @dev caller must approve this contract to spend vToken
     /// @notice vToken is locked within this contract prior to transfer of token
-    function redeem() override external {
-        uint vTokenBalance = IERC20(vToken).balanceOf(msg.sender);
+    function redeem() external override {
+        uint256 vTokenBalance = IERC20(vToken).balanceOf(msg.sender);
 
         /// ensure valid balance
         require(vTokenBalance > 0, "vKwentaRedeemer: No balance to redeem");
-        require(vTokenBalance <= IERC20(token).balanceOf(address(this)), 
+        require(
+            vTokenBalance <= IERC20(token).balanceOf(address(this)),
             "vKwentaRedeemer: Insufficient contract balance"
         );
 
         /// lock vToken in this contract
         require(
-            IERC20(vToken).transferFrom(msg.sender, address(this), vTokenBalance),
+            IERC20(vToken).transferFrom(
+                msg.sender,
+                address(this),
+                vTokenBalance
+            ),
             "vKwentaRedeemer: vToken transfer failed"
         );
 
