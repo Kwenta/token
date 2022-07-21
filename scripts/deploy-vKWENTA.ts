a353609295fede2ce52fc19952d0ee193573852c
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-07-21 09:52:55 -0400

    vkwenta deploy

--- scripts/deploy-vKWENTA.ts
+++ scripts/deploy-vKWENTA.ts
@@ -4,7 +4,8 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { wei } from "@synthetixio/wei";
-import { ethers } from "hardhat";
+import { Contract } from "ethers";
+import hre, { ethers } from "hardhat";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -22,14 +23,29 @@ async function main() {
   const vKwenta = await vKwentaFactory.deploy(
       "vKwenta", 
       "vKWENTA", 
-      "0x82d2242257115351899894eF384f779b5ba8c695", //treasury
+      "0x652c46a302060B324A02d2d3e4a56e3DA07FA91b", //treasury
       wei(313373).mul(.05).toBN()
     );
 
   await vKwenta.deployed();
 
+  saveDeployments('vKwenta', vKwenta);
   console.log("vKWENTA token deployed to:", vKwenta.address);
   console.log("Total supply is: ", wei(await vKwenta.totalSupply(), 18, true).toString());
+}
+
+async function saveDeployments(name: string, contract: Contract) {
+  // For hardhat-deploy plugin to save deployment artifacts
+  const { deployments } = hre;
+  const { save } = deployments;
+
+  const artifact = await deployments.getExtendedArtifact(name);
+  let deployment = {
+      address: contract.address,
+      ...artifact,
+  };
+
+  await save(name, deployment);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
