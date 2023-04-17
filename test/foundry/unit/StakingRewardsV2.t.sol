be45d55ea32d6f92d4c0c33675bf893afb198b7d
tommyrharper <thomasrobertharper@gmail.com>
2023-04-17 12:06:12 +0100

    ✅ StakingRewardsV2 test contract - testTokenSet

---
+++ test/foundry/unit/StakingRewardsV2.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../utils/Constants.sol";
+import {Kwenta} from "../../../contracts/Kwenta.sol";
+import {RewardEscrow} from "../../../contracts/RewardEscrow.sol";
+import {SupplySchedule} from "../../../contracts/SupplySchedule.sol";
+import {StakingRewardsV2} from "../../../contracts/StakingRewardsV2.sol";
+
+uint256 constant INITIAL_SUPPLY = 313373 ether;
+
+contract StakingRewardsV2Test is Test {
+    address public treasury;
+    Kwenta public kwenta;
+    RewardEscrow public rewardEscrow;
+    SupplySchedule public supplySchedule;
+    StakingRewardsV2 public stakingRewardsV2;
+
+    function setUp() public {
+        treasury = address(this);
+        kwenta = new Kwenta(
+            "Kwenta",
+            "KWENTA",
+            INITIAL_SUPPLY,
+            address(this),
+            treasury
+        );
+        rewardEscrow = new RewardEscrow(address(this), address(kwenta));
+        supplySchedule = new SupplySchedule(address(this), treasury);
+        kwenta.setSupplySchedule(address(supplySchedule));
+        stakingRewardsV2 = new StakingRewardsV2(
+            address(kwenta),
+            address(rewardEscrow),
+            address(supplySchedule)
+        );
+        supplySchedule.setStakingRewards(address(stakingRewardsV2));
+        rewardEscrow.setStakingRewards(address(stakingRewardsV2));
+
+        vm.prank(treasury);
+        kwenta.transfer(address(stakingRewardsV2), INITIAL_SUPPLY / 4);
+    }
+
+    function testTokenSet() public {
+        address token = address(stakingRewardsV2.token());
+        assertEq(token, address(kwenta));
+    }
+
+
+    function testOwner() public {
+        // address owner = stakingRewardsV2.owner();
+        // assertEq(owner, address(this));
+    }
+}
