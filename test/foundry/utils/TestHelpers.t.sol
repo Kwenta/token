2e7c40d76027a7b30889bd5bb704a1638def65f6
tommyrharper <thomasrobertharper@gmail.com>
2023-04-17 12:58:07 +0100

    ✅ add TestHelpers with createUser helper

---
+++ test/foundry/utils/TestHelpers.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {Kwenta} from "../../../contracts/Kwenta.sol";
+import {RewardEscrow} from "../../../contracts/RewardEscrow.sol";
+import {SupplySchedule} from "../../../contracts/SupplySchedule.sol";
+import {StakingRewardsV2} from "../../../contracts/StakingRewardsV2.sol";
+
+uint256 constant INITIAL_SUPPLY = 313373 ether;
+
+contract TestHelpers is Test {
+    uint256 public userNonce;
+
+    function createUser() public returns (address) {
+        userNonce++;
+        return vm.addr(userNonce);
+    }
+}
