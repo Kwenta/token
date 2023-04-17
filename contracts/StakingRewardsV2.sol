6ecbd50a4e9d29a9b29c2eeeb61f80a8a7752e68
tommyrharper <thomasrobertharper@gmail.com>
2023-04-17 20:07:32 +0100

    🗑️ Remove forge-std import

--- contracts/StakingRewardsV2.sol
+++ contracts/StakingRewardsV2.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
