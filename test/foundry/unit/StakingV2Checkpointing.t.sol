3dd4e6184db3b5c30cdd27ee2c7d5a6b9c04955f
tommyrharper <thomasrobertharper@gmail.com>
2023-04-18 15:26:39 +0100

    ✅ testBalancesCheckpointsAreUpdatedFuzz

--- test/foundry/unit/StakingV2Checkpointing.t.sol
+++ test/foundry/unit/StakingV2Checkpointing.t.sol
@@ -20,8 +20,10 @@ contract StakingV2CheckpointingTests is StakingRewardsTestHelpers {
         stakingRewardsV2.stake(TEST_VALUE);
 
         // get last checkpoint
-        (uint256 blockNum, uint256 value) = stakingRewardsV2
-            .balances(address(this), 0);
+        (uint256 blockNum, uint256 value) = stakingRewardsV2.balances(
+            address(this),
+            0
+        );
 
         // check values
         assertEq(blockNum, block.number);
@@ -44,7 +46,56 @@ contract StakingV2CheckpointingTests is StakingRewardsTestHelpers {
         assertEq(value, 0);
     }
 
-    // function testBalancesCheckpointsAreUpdatedFuzz(uint256 numberOf) public {
+    function testBalancesCheckpointsAreUpdatedFuzz(
+        uint32 amount,
+        uint32 blockAdvance,
+        uint8 numberOfRounds
+    ) public {
+        vm.assume(amount > 0);
 
-    // }
+        for (uint8 i = 0; i < numberOfRounds; i++) {
+            _testBalancesCheckpointsAreUpdatedFuzz(amount, blockAdvance);
+        }
+    }
+
+    function _testBalancesCheckpointsAreUpdatedFuzz(
+        uint256 amount,
+        uint256 blockAdvance
+    ) public {
+        uint256 length = stakingRewardsV2.balancesLength(address(this));
+        uint256 previousTotal = stakingRewardsV2.balanceOf(address(this));
+
+        // stake
+        fundAndApproveAccount(address(this), amount);
+        stakingRewardsV2.stake(amount);
+
+        // get last checkpoint
+        (uint256 blockNum, uint256 value) = stakingRewardsV2.balances(
+            address(this),
+            length
+        );
+
+        // check values
+        assertEq(blockNum, block.number);
+        assertEq(value, previousTotal + amount);
+
+        // move beyond cold period
+        vm.warp(block.timestamp + stakingRewardsV2.unstakingCooldownPeriod());
+
+        // update block number
+        vm.roll(block.number + blockAdvance);
+
+        // // unstake
+        stakingRewardsV2.unstake(amount);
+
+        // get last checkpoint
+        (blockNum, value) = stakingRewardsV2.balances(
+            address(this),
+            length + 1
+        );
+
+        // check values
+        assertEq(blockNum, block.number);
+        assertEq(value, previousTotal);
+    }
 }
