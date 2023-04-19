c1ed736f561e77b2262b74598e7f9e1212feec99
tommyrharper <thomasrobertharper@gmail.com>
2023-04-19 18:29:06 +0100

    👷🏻 RewardsEscrowV2 initial base

---
+++ contracts/interfaces/IRewardEscrowV2.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library VestingEntries {
+    struct VestingEntry {
+        uint64 endTime;
+        uint256 escrowAmount;
+        uint256 duration;
+    }
+    struct VestingEntryWithID {
+        uint64 endTime;
+        uint256 escrowAmount;
+        uint256 entryID;
+    }
+}
+
+interface IRewardEscrowV2 {
+    // Views
+    function getKwentaAddress() external view returns (address);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function numVestingEntries(address account) external view returns (uint256);
+
+    function totalEscrowedAccountBalance(address account)
+        external
+        view
+        returns (uint256);
+
+    function totalVestedAccountBalance(address account)
+        external
+        view
+        returns (uint256);
+
+    function getVestingQuantity(address account, uint256[] calldata entryIDs)
+        external
+        view
+        returns (uint256, uint256);
+
+    function getVestingSchedules(
+        address account,
+        uint256 index,
+        uint256 pageSize
+    ) external view returns (VestingEntries.VestingEntryWithID[] memory);
+
+    function getAccountVestingEntryIDs(
+        address account,
+        uint256 index,
+        uint256 pageSize
+    ) external view returns (uint256[] memory);
+
+    function getVestingEntryClaimable(address account, uint256 entryID)
+        external
+        view
+        returns (uint256, uint256);
+
+    function getVestingEntry(address account, uint256 entryID)
+        external
+        view
+        returns (
+            uint64,
+            uint256,
+            uint256
+        );
+
+    // Mutative functions
+    function vest(uint256[] calldata entryIDs) external;
+
+    function createEscrowEntry(
+        address beneficiary,
+        uint256 deposit,
+        uint256 duration
+    ) external;
+
+    function appendVestingEntry(
+        address account,
+        uint256 quantity,
+        uint256 duration
+    ) external;
+
+    function stakeEscrow(uint256 _amount) external;
+
+    function unstakeEscrow(uint256 _amount) external;
+}
