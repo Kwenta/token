f8b81e42ae453dc6aa19154687271ed40897f71b
tommyrharper <thomasrobertharper@gmail.com>
2023-04-24 20:42:44 +0100

    🚀 Add pauseAndSwitchToStakingRewardsV2 to Setup

--- scripts/Migrate.s.sol
+++ scripts/Migrate.s.sol
@@ -11,10 +11,13 @@ import {StakingRewardsV2} from "../../../contracts/StakingRewardsV2.sol";
 /// @title Script to migration from StakingV1 to StakingV2
 /// @author tommyrharper (tom@solidity.ninja)
 contract Setup {
-    function deploySystem(address _owner, address _kwenta, address _supplySchedule, address _stakingRewardsV1)
-        public
-        returns (RewardEscrowV2 rewardEscrowV2, StakingRewardsV2 stakingRewardsV2)
-    {
+    function deploySystem(
+        address _owner,
+        address _kwenta,
+        address _supplySchedule,
+        address _stakingRewardsV1,
+        bool _pauseAndMigrate
+    ) public returns (RewardEscrowV2 rewardEscrowV2, StakingRewardsV2 stakingRewardsV2) {
         RewardEscrowV2 rewardEscrowV2 = new RewardEscrowV2(_owner, _kwenta);
         StakingRewardsV2 stakingRewardsV2 = new StakingRewardsV2(
             _kwenta,
@@ -23,17 +26,37 @@ contract Setup {
             address(_stakingRewardsV1)
         );
 
+        console.log("Deployed StakingRewardsV2 at %s", address(stakingRewardsV2));
+        console.log("Deployed RewardEscrowV2 at %s", address(rewardEscrowV2));
+
+        if (_pauseAndMigrate) {
+            pauseAndSwitchToStakingRewardsV2(
+                _stakingRewardsV1, _supplySchedule, address(stakingRewardsV2), address(rewardEscrowV2)
+            );
+        }
+
+        return (rewardEscrowV2, stakingRewardsV2);
+    }
+
+    function pauseAndSwitchToStakingRewardsV2(
+        address _stakingRewardsV1,
+        address _supplySchedule,
+        address _stakingRewardsV2,
+        address _rewardEscrowV2
+    ) public {
         StakingRewards stakingRewardsV1 = StakingRewards(_stakingRewardsV1);
         SupplySchedule supplySchedule = SupplySchedule(_supplySchedule);
+        StakingRewardsV2 stakingRewardsV2 = StakingRewardsV2(_stakingRewardsV2);
+        RewardEscrowV2 rewardEscrowV2 = RewardEscrowV2(_rewardEscrowV2);
 
-        // // Pause StakingV1
-        // stakingRewardsV1.pauseStakingRewards();
+        // Pause StakingV1
+        stakingRewardsV1.pauseStakingRewards();
 
-        // // Update SupplySchedule to point to StakingV2
-        // supplySchedule.setStakingRewards(address(stakingRewardsV2));
-        // rewardEscrowV2.setStakingRewards(address(stakingRewardsV2));
+        // Update SupplySchedule to point to StakingV2
+        supplySchedule.setStakingRewards(address(stakingRewardsV2));
+        rewardEscrowV2.setStakingRewards(address(stakingRewardsV2));
 
-        // // Unpause StakingV1
-        // stakingRewardsV1.unpauseStakingRewards();
+        // Unpause StakingV1
+        stakingRewardsV1.unpauseStakingRewards();
     }
 }
