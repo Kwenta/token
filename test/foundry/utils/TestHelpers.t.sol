1ad9894df123b238e1587b8d4c85101c55604d12
tommyrharper <thomasrobertharper@gmail.com>
2023-04-24 18:57:52 +0100

    📚 Add natspec to test helpers

--- test/foundry/utils/TestHelpers.t.sol
+++ test/foundry/utils/TestHelpers.t.sol
@@ -7,11 +7,16 @@ contract TestHelpers is Test {
     uint256 public userNonce;
     uint256 public nonce;
 
+    /// @dev create a new user address
     function createUser() public returns (address) {
         userNonce++;
         return vm.addr(userNonce);
     }
 
+    /// @dev assert two numbers are close to each other
+    /// @param a first number
+    /// @param b second number
+    /// @param tolerance maximum difference between a and b allowed
     function assertCloseTo(uint256 a, uint256 b, uint256 tolerance) public {
         if (tolerance == 0) {
             assertEq(a, b);
@@ -22,26 +27,43 @@ contract TestHelpers is Test {
         }
     }
 
+    /// @dev check if two numbers are close to each other
+    /// @param a first number
+    /// @param b second number
+    /// @param tolerance maximum difference between a and b allowed
+    /// @return result true if a and b are close to each other within the tolerance
     function closeTo(uint256 a, uint256 b, uint256 tolerance) public pure returns (bool) {
         if (a == b) return true;
         if (a > b) return a - b <= tolerance;
         else return b - a <= tolerance;
     }
 
+    /// @dev get minimum of two numbers
+    /// @param a first number
+    /// @param b second number
+    /// @return result minimum of a and b
     function min(uint256 a, uint256 b) public pure returns (uint256) {
         return a < b ? a : b;
     }
 
-    // get psuedorandom bool
+    /// @dev  get psuedorandom bool
+    /// @return result psuedorandom bool
     function flipCoin() public returns (bool) {
         return (uint256(keccak256(abi.encodePacked(++nonce))) % 2) == 1;
     }
 
-    // get psuedorandom bool with salt
+    /// @dev get psuedorandom bool with salt
+    /// @param salt salt to influence random number
+    /// @return result psuedorandom bool
     function flipCoin(uint256 salt) public returns (bool) {
         return (uint256(keccak256(abi.encodePacked(++nonce, salt))) % 2) == 1;
     }
 
+    /// @dev get psuedorandom uint256
+    /// @param _max max value
+    /// @param _min min value
+    /// @param salt salt to influence random number
+    /// @return result psuedorandom number
     function getPseudoRandomNumber(uint256 _max, uint256 _min, uint256 salt) internal returns (uint256 result) {
         require(_min <= _max, "min must be <= max");
         if (_max == _min) return _max;
