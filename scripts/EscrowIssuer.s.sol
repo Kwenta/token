df10e0db65de3c6cfc127c26883c5e65e5d9e578
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-05-09 15:55:13 -0400

    👷 Add deployment parameters

--- scripts/EscrowIssuer.s.sol
+++ scripts/EscrowIssuer.s.sol
@@ -4,24 +4,21 @@ pragma solidity ^0.8.19;
 import "forge-std/Script.sol";
 import {EscrowIssuer} from "../contracts/EscrowIssuer.sol";
 
-contract MyScript is Script {
-    address public kwenta;
-
-    address public rewardEscrow;
-
-    //public address
-    address public treasury = 0x3C704e28C8EfCC7aCa262031818001895595081D;
-
-    function run() external {
+contract DeployEscrowIssuer is Script {
+    function deployEscrowIssuer(
+        address _kwenta,
+        address _rewardEscrow,
+        address _treasury
+    ) public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         EscrowIssuer escrowIssuer = new EscrowIssuer(
             "EscIss",
             "EIS",
-            address(kwenta),
-            address(rewardEscrow),
-            address(treasury)
+            address(_kwenta),
+            address(_rewardEscrow),
+            address(_treasury)
         );
 
         vm.stopBroadcast();
