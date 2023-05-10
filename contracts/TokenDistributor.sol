cc6c6b6dc66cdac42ef38e362cb2bef78d2cf1ab
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-05-10 11:03:15 -0400

    📚 Comments

--- contracts/TokenDistributor.sol
+++ contracts/TokenDistributor.sol
@@ -19,11 +19,23 @@ contract TokenDistributor {
      *   can only be called once per week
      *   consider calling this the first time someone tries to claim in a new epoch
      */
-    function newDistribution() public {}
+    function newDistribution() public {
+
+        //require there hasnt been a distribution this week yet "TokenDistributor: Distribution already happened this week"
+
+        //create new distribution
+
+        //map this to uint (block or sequential distribution epochs?)
+
+    }
 
     /**
      *   this function will fetch StakingRewardsV2 to see what their
      *   staked balance was at the start of the epoch
      */
-    function claimDistribution(address to, uint epochNumber) public {}
+    function claimDistribution(address to, uint epochNumber) public {
+
+        //fetch staked balance from StakingRewardsV2
+
+    }
 }
