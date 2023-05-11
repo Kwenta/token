03cf89459afbd40516244cbb9bd44290b728fd82
tommyrharper <thomasrobertharper@gmail.com>
2023-05-11 21:17:34 +0100

    ✅ Initial getRewardOnBehalf test

---
+++ test/foundry/unit/StakingRewardsOnBehalfActions.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {DefaultStakingV2Setup} from "../utils/DefaultStakingV2Setup.t.sol";
+import {StakingRewardsV2} from "../../../contracts/StakingRewardsV2.sol";
+import "../utils/Constants.t.sol";
+
+contract StakingRewardsOnBehalfActionsTests is DefaultStakingV2Setup {
+    /*//////////////////////////////////////////////////////////////
+                        Get Reward On Behalf
+    //////////////////////////////////////////////////////////////*/
+
+    function test_getRewardOnBehalf() public {
+        fundAccountAndStakeV2(address(this), TEST_VALUE);
+
+        // assert initial rewards are 0
+        assertEq(rewardEscrowV2.balanceOf(address(this)), 0);
+        assertEq(rewardEscrowV2.balanceOf(user1), 0);
+
+        // send in 604800 (1 week) of rewards - (using 1 week for round numbers)
+        addNewRewardsToStakingRewardsV2(1 weeks);
+
+        // fast forward 1 week - one complete period
+        vm.warp(block.timestamp + stakingRewardsV2.rewardsDuration());
+
+        // approve operator
+        stakingRewardsV2.approveOperator(user1, true);
+
+        // claim rewards on behalf
+        vm.prank(user1);
+        stakingRewardsV2.getRewardOnBehalf(address(this));
+
+        // check rewards
+        assertEq(rewardEscrowV2.balanceOf(address(this)), 1 weeks);
+        assertEq(rewardEscrowV2.balanceOf(user1), 0);
+    }
+
+    // TODO: test using it without approval throws error
+    // TODO: test offering approval emits an event
+    // TODO: test claiming rewards on behalf emits an event
+}
