98959a3cc1eac68cf0dc6d61debd21f984c62b2d
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-05-11 15:38:06 -0400

    ✅ claimDistribution tests

--- test/foundry/unit/TokenDistributor.t.sol
+++ test/foundry/unit/TokenDistributor.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
 import {TokenDistributor} from "../../../contracts/TokenDistributor.sol";
@@ -13,12 +13,16 @@ contract TokenDistributorTest is Test {
     Kwenta public kwenta;
     StakingRewardsV2 public stakingRewardsV2;
     address public user;
+    address public user2;
 
     function setUp() public {
         user = payable(
             address(uint160(uint256(keccak256(abi.encodePacked("user")))))
         );
-        kwenta = new Kwenta("Kwenta", "Kwe", 11, address(this), address(this));
+        user2 = payable(
+            address(uint160(uint256(keccak256(abi.encodePacked("user2")))))
+        );
+        kwenta = new Kwenta("Kwenta", "Kwe", 15, address(this), address(this));
         /// @dev kwenta is plugged in for all parameters to control variables
         /// @dev functions that are used by TokenDistributor shouldn't need the other dependencies
         stakingRewardsV2 = new StakingRewardsV2(
@@ -51,6 +55,20 @@ contract TokenDistributorTest is Test {
         tokenDistributor.newDistribution();
     }
 
+    /// @notice newDistribution fail - last epoch hasnt ended yet, epoch > 1
+    function testNewDistributionLastEpochNotEnded2() public {
+        vm.startPrank(user);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 604801);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 604801);
+        tokenDistributor.newDistribution();
+        vm.expectRevert(
+            "TokenDistributor: Last week's epoch has not ended yet"
+        );
+        tokenDistributor.newDistribution();
+    }
+
     /// @notice newDistribution test first epoch and second
     function testNewDistributionSequentialEpochs() public {
         vm.startPrank(user);
@@ -59,44 +77,100 @@ contract TokenDistributorTest is Test {
         vm.expectEmit(true, true, true, true);
         emit NewEpochCreated(604802, 1);
         tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 604801);
+        vm.expectEmit(true, true, true, true);
+        emit NewEpochCreated(1209603, 2);
+        tokenDistributor.newDistribution();
+    }
+
+    /// @notice newDistribution test fail after 2 created and third is too soon
+    function testNewDistributionSequentialEpochsFail() public {
+        vm.startPrank(user);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 604801);
+        vm.expectEmit(true, true, true, true);
+        emit NewEpochCreated(604802, 1);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 304801);
+        vm.expectRevert("TokenDistributor: Last week's epoch has not ended yet");
+        tokenDistributor.newDistribution();
     }
 
     /// @notice claimDistribution happy case and make a new epoch
     function testClaimDistributionNewEpoch() public {
-        //make sure tokenDistributor has kwenta first
+        //setup
         kwenta.transfer(address(tokenDistributor), 10);
-        //then have user stake in stakingRewardsV2
         kwenta.transfer(address(user), 1);
         vm.prank(address(kwenta));
         stakingRewardsV2.stakeEscrow(address(user), 1);
-        //then make new epoch
         vm.prank(user);
         tokenDistributor.newDistribution();
-        //wait a week
         vm.warp(block.timestamp + 604801);
-        //claim distribution
-        vm.prank(user);
-        tokenDistributor.claimDistribution(address(user), 0);
-        //assert that its actually a new epoch
 
+        vm.prank(user);
+        vm.expectEmit(true, true, true, true);
+        emit NewEpochCreated(604802, 1);
+        tokenDistributor.claimDistribution(address(user), 0);
+
+        //todo: change to rewardEscrow outcome
         //assert that user got all the fees
         assertEq(kwenta.balanceOf(user), 11);
     }
 
     /// @notice claimDistribution happy case and don't make a new epoch
-    function testClaimDistribution() public {}
+    function testClaimDistributionSameEpoch() public {
+        //setup
+        kwenta.transfer(address(tokenDistributor), 10);
+        kwenta.transfer(address(user), 1);
+        kwenta.transfer(address(user2), 1);
+        vm.startPrank(address(kwenta));
+        stakingRewardsV2.stakeEscrow(address(user), 1);
+        stakingRewardsV2.stakeEscrow(address(user2), 1);
+        vm.stopPrank();
+        vm.prank(user);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 604801);
+
+        vm.prank(user);
+        vm.expectEmit(true, true, true, true);
+        emit NewEpochCreated(604802, 1);
+        tokenDistributor.claimDistribution(address(user), 0);
+
+        vm.warp(block.timestamp + 1000);
+        vm.prank(user2);
+        tokenDistributor.claimDistribution(address(user2), 0);
+
+
+    }
 
     /// @notice claimDistribution fail - epoch is not ready to claim
     function testClaimDistributionEpochNotReady() public {
-        //do a revert with message
+        vm.startPrank(user);
+        tokenDistributor.newDistribution();
+        vm.warp(block.timestamp + 304801);
+        vm.expectRevert("TokenDistributor: Epoch is not ready to claim");
+        tokenDistributor.claimDistribution(address(user), 0);
+    }
+
+    /// @notice claimDistribution fail - epoch is not ready to claim, not an epoch yet
+    function testClaimDistributionEpochAhead() public {
+        vm.startPrank(user);
+        tokenDistributor.newDistribution();
+        vm.expectRevert("TokenDistributor: Epoch is not ready to claim");
+        tokenDistributor.claimDistribution(address(user), 7);
+    }
+
+    /// @notice claimDistribution fail - no epoch to claim yet
+    function testClaimDistributionNoEpochYet() public {
+        vm.startPrank(user);
+        vm.expectRevert();
+        tokenDistributor.claimDistribution(address(user), 0);
     }
 
     /// @notice claimDistribution fail - already claimed
     function testClaimDistributionAlreadyClaimed() public {
-        //expect revert with message
+        
     }
-
-    //maybe try fuzzing claimDistribution
 
     /// @notice claimDistribution with previous claims in earlier epochs
 
