ed4349ee8fcbe499fda111e56ccb2242e6ed6f4e
tommyrharper <thomasrobertharper@gmail.com>
2023-05-19 19:17:56 +0100

    ✅ Fix TokenDistributorTest setup

--- test/foundry/unit/TokenDistributor.t.sol
+++ test/foundry/unit/TokenDistributor.t.sol
@@ -5,10 +5,10 @@ import {TokenDistributor} from "../../../contracts/TokenDistributor.sol";
 import {Kwenta} from "../../../contracts/Kwenta.sol";
 import {StakingRewardsV2} from "../../../contracts/StakingRewardsV2.sol";
 import {RewardEscrowV2} from "../../../contracts/RewardEscrowV2.sol";
-import {TestHelpers} from "../utils/TestHelpers.t.sol";
+import {StakingSetup} from "../utils/StakingSetup.t.sol";
 import "forge-std/Test.sol";
 
-contract TokenDistributorTest is TestHelpers {
+contract TokenDistributorTest is StakingSetup {
     event CheckpointToken(uint time, uint tokens);
     event VestingEntryCreated(
         address indexed beneficiary,
@@ -18,49 +18,26 @@ contract TokenDistributorTest is TestHelpers {
     );
 
     TokenDistributor public tokenDistributor;
-    Kwenta public kwenta;
-    StakingRewardsV2 public stakingRewardsV2;
-    RewardEscrowV2 public rewardEscrowV2;
-    address public user;
-    address public user2;
 
-    function setUp() public {
+    function setUp() public override {
         /// @dev starts after a week so the startTime is != 0
         goForward(604801);
-        user = payable(
-            address(uint160(uint256(keccak256(abi.encodePacked("user")))))
-        );
-        user2 = payable(
-            address(uint160(uint256(keccak256(abi.encodePacked("user2")))))
-        );
-        kwenta = new Kwenta(
-            "Kwenta",
-            "Kwe",
-            100000,
-            address(this),
-            address(this)
-        );
-        rewardEscrowV2 = new RewardEscrowV2(address(this), address(kwenta));
-        /// @dev kwenta is plugged in for all parameters except rewardEscrowV2 to control variables
-        /// functions that are used by TokenDistributor shouldn't need the other dependencies
-        stakingRewardsV2 = new StakingRewardsV2(
-            address(kwenta),
-            address(rewardEscrowV2),
-            address(kwenta),
-            address(kwenta)
-        );
+        super.setUp();
+        switchToStakingV2();
         tokenDistributor = new TokenDistributor(
             address(kwenta),
             address(stakingRewardsV2),
             address(rewardEscrowV2)
         );
+        vm.prank(treasury);
+        kwenta.transfer(address(this), 100_000 ether);
     }
 
     /// @notice checkpointToken happy case after 1 week
     function testCheckpointToken() public {
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         goForward(604801);
@@ -74,8 +51,8 @@ contract TokenDistributorTest is TestHelpers {
     function testClaimEpoch() public {
         //setup
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         goForward(604801);
@@ -84,35 +61,35 @@ contract TokenDistributorTest is TestHelpers {
         emit CheckpointToken(1209603, 10);
         vm.expectEmit(true, true, false, true);
         //todo: figure out why the amount is 9 not 10
-        emit VestingEntryCreated(address(user), 9, 31449600, 1);
-        tokenDistributor.claimEpoch(address(user), 0);
+        emit VestingEntryCreated(address(user1), 9, 31449600, 1);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
 
     /// @notice claimDistribution fail - epoch is not ready to claim
     function testClaimDistributionEpochNotReady() public {
-        vm.startPrank(user);
+        vm.startPrank(user1);
 
         goForward(304801);
         vm.expectRevert(
             abi.encodeWithSelector(TokenDistributor.CannotClaimYet.selector)
         );
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
 
     /// @notice claimDistribution fail - epoch is not ready to claim, not an epoch yet
     function testClaimDistributionEpochAhead() public {
-        vm.startPrank(user);
+        vm.startPrank(user1);
         vm.expectRevert(
             abi.encodeWithSelector(TokenDistributor.CannotClaimYet.selector)
         );
-        tokenDistributor.claimEpoch(address(user), 7);
+        tokenDistributor.claimEpoch(address(user1), 7);
     }
 
     /// @notice claimDistribution fail - no epoch to claim yet
     function testClaimDistributionNoEpochYet() public {
-        vm.startPrank(user);
+        vm.startPrank(user1);
         vm.expectRevert();
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
 
     //todo: comment out until figure out if doing things in same block matters
@@ -120,8 +97,8 @@ contract TokenDistributorTest is TestHelpers {
     /// @notice claimDistribution fail - cant claim in same block as new distribution
     function testClaimDistributionNewDistributionBlock() public {
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         goForward(604801);
@@ -129,38 +106,38 @@ contract TokenDistributorTest is TestHelpers {
         vm.expectRevert(
             abi.encodeWithSelector(TokenDistributor.CannotClaimInNewEpochBlock.selector)
         );
-        tokenDistributor.claimEpoch(address(user), 1);
+        tokenDistributor.claimEpoch(address(user1), 1);
     }
     */
     /// @notice claimDistribution fail - already claimed
     function testClaimDistributionAlreadyClaimed() public {
         //setup
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         goForward(604801);
 
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
         vm.expectRevert(
             abi.encodeWithSelector(TokenDistributor.CannotClaimTwice.selector)
         );
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
     
     /// @notice claimDistribution fail - claim an epoch that had no staking
     function testClaimDistributionNoStaking() public {
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(user);
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(user1);
         goForward(604801);
         vm.expectRevert(
             abi.encodeWithSelector(
                 TokenDistributor.NothingStakedThatEpoch.selector
             )
         );
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
     
     /// @notice claimDistribution fail - nonstaker tries to claim
@@ -168,8 +145,8 @@ contract TokenDistributorTest is TestHelpers {
     function testClaimDistributionNotStaker() public {
         //setup
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         goForward(604801);
@@ -187,20 +164,20 @@ contract TokenDistributorTest is TestHelpers {
     function testClaimDistributionPreviouslyStaked() public {
         //setup
         kwenta.transfer(address(tokenDistributor), 10);
-        kwenta.transfer(address(user), 1);
-        vm.startPrank(address(user));
+        kwenta.transfer(address(user1), 1);
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         uint here = block.timestamp;
-        stakingRewardsV2.balanceAtTime(address(user), here);
+        stakingRewardsV2.balanceAtTime(address(user1), here);
         stakingRewardsV2.totalSupplyAtTime(here);
 
         goForward(1209601);
         stakingRewardsV2.unstake(1);
-        stakingRewardsV2.balanceAtTime(address(user), here);
+        stakingRewardsV2.balanceAtTime(address(user1), here);
         stakingRewardsV2.totalSupplyAtTime(here);
         goForward(604801);
-        tokenDistributor.claimEpoch(address(user), 0);
+        tokenDistributor.claimEpoch(address(user1), 0);
     }
     //
     //temporarily comment out every other test for compiling
@@ -210,13 +187,13 @@ contract TokenDistributorTest is TestHelpers {
     /// in earlier epochs 2 complete epochs with differing fees
     /// @dev also an integration test with RewardEscrowV2
     function testClaimDistributionMultipleClaims() public {
-        /// @dev user has 1/3 total staking and user2 has 2/3
+        /// @dev user1 has 1/3 total staking and user2 has 2/3
         /// before epoch #0 (same as during) TokenDistributor
         /// receives 1000 in fees
         kwenta.transfer(address(tokenDistributor), 1000);
-        kwenta.transfer(address(user), 1);
+        kwenta.transfer(address(user1), 1);
         kwenta.transfer(address(user2), 2);
-        vm.startPrank(address(user));
+        vm.startPrank(address(user1));
         kwenta.approve(address(stakingRewardsV2), 1);
         stakingRewardsV2.stake(1);
         vm.stopPrank();
@@ -224,33 +201,33 @@ contract TokenDistributorTest is TestHelpers {
         kwenta.approve(address(stakingRewardsV2), 2);
         stakingRewardsV2.stake(2);
         vm.stopPrank();
-        vm.prank(user);
+        vm.prank(user1);
         tokenDistributor.newDistribution();
         goForward(604801);
 
-        /// @dev during epoch #1, user claims their fees from #0
+        /// @dev during epoch #1, user1 claims their fees from #0
         /// and TokenDistributor receives 5000 in fees
 
         vm.expectEmit(true, true, true, true);
         emit NewEpochCreated(604802, 1);
         tokenDistributor.newDistribution();
         kwenta.transfer(address(tokenDistributor), 5000);
-        vm.prank(user);
+        vm.prank(user1);
         vm.expectEmit(true, true, false, true);
-        emit VestingEntryCreated(address(user), 333, 31449600, 1);
-        tokenDistributor.claimDistribution(address(user), 0);
+        emit VestingEntryCreated(address(user1), 333, 31449600, 1);
+        tokenDistributor.claimDistribution(address(user1), 0);
 
-        /// @dev user claims for epoch #1 to start epoch #2
+        /// @dev user1 claims for epoch #1 to start epoch #2
         /// user2 also claims for #1 and #0
         /// and TokenDistributor receives 300 in fees
 
         goForward(604801);
-        vm.prank(user);
+        vm.prank(user1);
         vm.expectEmit(true, true, true, true);
         emit NewEpochCreated(1209603, 2);
         vm.expectEmit(true, true, false, true);
-        emit VestingEntryCreated(address(user), 1666, 31449600, 2);
-        tokenDistributor.claimDistribution(address(user), 1);
+        emit VestingEntryCreated(address(user1), 1666, 31449600, 2);
+        tokenDistributor.claimDistribution(address(user1), 1);
         goForward(1000);
         kwenta.transfer(address(tokenDistributor), 300);
         vm.prank(user2);
