efb104ea8d105afdcfd8419bf36173d73dfa3bd5
tommyrharper <thomasrobertharper@gmail.com>
2023-05-25 16:44:38 +0100

    ✅ InvalidDuration test

--- test/foundry/unit/RewardEscrowV2.t.sol
+++ test/foundry/unit/RewardEscrowV2.t.sol
@@ -89,4 +89,12 @@ contract RewardEscrowV2Tests is DefaultStakingV2Setup {
         vm.expectRevert(IRewardEscrowV2.OnlyStakingRewards.selector);
         rewardEscrowV2.appendVestingEntry(address(this), 1 ether, 52 weeks);
     }
+
+    function test_Should_Revert_If_Duration_Is_0() public {
+        vm.prank(treasury);
+        kwenta.transfer(address(rewardEscrowV2), 1 ether);
+        vm.prank(address(stakingRewardsV2));
+        vm.expectRevert(IRewardEscrowV2.InvalidDuration.selector);
+        rewardEscrowV2.appendVestingEntry(address(this), 1 ether, 0);
+    }
 }
