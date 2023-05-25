207a1819afeb126a38ace7c09f7981ab1f6c70eb
tommyrharper <thomasrobertharper@gmail.com>
2023-05-25 15:04:50 +0100

    🗑️ Delete unused StakingRewardsV2 Owned import

--- contracts/StakingRewardsV2.sol
+++ contracts/StakingRewardsV2.sol
@@ -7,7 +7,6 @@ import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "./utils/Owned.sol";
 import "./interfaces/IStakingRewardsV2.sol";
 import "./interfaces/IStakingRewards.sol";
 import "./interfaces/ISupplySchedule.sol";
