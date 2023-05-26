dd623bc7326c8ea9cf7b87f669bc77915d508d74
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-05-26 15:33:29 -0400

    ✨ lint

--- test/foundry/unit/TokenDistributor.t.sol
+++ test/foundry/unit/TokenDistributor.t.sol
@@ -711,7 +711,12 @@ contract TokenDistributorTest is StakingSetup {
     }
 
     /// @notice test fuzz staking with a custom offset
-    function testFuzzStakingOffset(uint amount, uint staking1, uint staking2, uint staking3) public {
+    function testFuzzStakingOffset(
+        uint amount,
+        uint staking1,
+        uint staking2,
+        uint staking3
+    ) public {
         TokenDistributor tokenDistributorOffset = new TokenDistributor(
             address(kwenta),
             address(stakingRewardsV2),
@@ -761,26 +766,35 @@ contract TokenDistributorTest is StakingSetup {
 
         /// @dev claim at the start of the new epoch (should also checkpoint)
         goForward(1 weeks);
-        uint proportionalFees1 = amount * staking1 / (staking1 + staking2 + staking3);
+        uint proportionalFees1 = (amount * staking1) /
+            (staking1 + staking2 + staking3);
         vm.expectEmit(true, true, true, true);
         emit CheckpointToken(1382400, amount);
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user1), 1, proportionalFees1);
         tokenDistributorOffset.claimEpoch(address(user1), 1);
 
-        uint proportionalFees2 = amount * staking2 / (staking1 + staking2 + staking3);
+        uint proportionalFees2 = (amount * staking2) /
+            (staking1 + staking2 + staking3);
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user2), 1, proportionalFees2);
         tokenDistributorOffset.claimEpoch(address(user2), 1);
 
-        uint proportionalFees3 = amount * staking3 / (staking1 + staking2 + staking3);
+        uint proportionalFees3 = (amount * staking3) /
+            (staking1 + staking2 + staking3);
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user3), 1, proportionalFees3);
         tokenDistributorOffset.claimEpoch(address(user3), 1);
     }
 
     /// @notice test fuzz time with a custom offset
-    function testFuzzTimeOffset(uint amount, uint staking1, uint staking2, uint staking3, uint time) public {
+    function testFuzzTimeOffset(
+        uint amount,
+        uint staking1,
+        uint staking2,
+        uint staking3,
+        uint time
+    ) public {
         TokenDistributor tokenDistributorOffset = new TokenDistributor(
             address(kwenta),
             address(stakingRewardsV2),
@@ -835,24 +849,25 @@ contract TokenDistributorTest is StakingSetup {
         /// @dev claim at the start of the new epoch (should also checkpoint)
         goForward(1 weeks);
         goForward(time);
-        uint proportionalFees1 = (((amount * 1 weeks) / (time + 1 weeks)) * staking1 / (staking1 + staking2 + staking3));
+        uint proportionalFees1 = ((((amount * 1 weeks) / (time + 1 weeks)) *
+            staking1) / (staking1 + staking2 + staking3));
         vm.expectEmit(true, true, true, true);
         emit CheckpointToken(1382400 + time, amount);
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user1), 1, proportionalFees1);
         tokenDistributorOffset.claimEpoch(address(user1), 1);
 
-        
-        uint proportionalFees2 = (((amount * 1 weeks) / (time + 1 weeks)) * staking2 / (staking1 + staking2 + staking3));
+        uint proportionalFees2 = ((((amount * 1 weeks) / (time + 1 weeks)) *
+            staking2) / (staking1 + staking2 + staking3));
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user2), 1, proportionalFees2);
         tokenDistributorOffset.claimEpoch(address(user2), 1);
-        
-        uint proportionalFees3 = (((amount * 1 weeks) / (time + 1 weeks)) * staking3 / (staking1 + staking2 + staking3));
+
+        uint proportionalFees3 = ((((amount * 1 weeks) / (time + 1 weeks)) *
+            staking3) / (staking1 + staking2 + staking3));
         vm.expectEmit(true, true, true, true);
         emit EpochClaim(address(user3), 1, proportionalFees3);
         tokenDistributorOffset.claimEpoch(address(user3), 1);
-        
     }
 
     /// @notice test startOfWeek
