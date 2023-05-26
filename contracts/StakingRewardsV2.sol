443f9d85f8d9fe686788297a1b5a953563ff20a4
tommyrharper <thomasrobertharper@gmail.com>
2023-05-26 16:10:06 +0100

    ✨ Lint StakingRewardsV2

--- contracts/StakingRewardsV2.sol
+++ contracts/StakingRewardsV2.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.19;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import {PausableUpgradeable} from
+    "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
@@ -628,11 +630,7 @@ contract StakingRewardsV2 is
 
     /// @notice set unstaking cooldown period
     /// @param _cooldownPeriod: denoted in seconds
-    function setCooldownPeriod(uint256 _cooldownPeriod)
-        external
-        override
-        onlyOwner
-    {
+    function setCooldownPeriod(uint256 _cooldownPeriod) external override onlyOwner {
         if (_cooldownPeriod < MIN_COOLDOWN_PERIOD) {
             revert CooldownPeriodTooLow(MIN_COOLDOWN_PERIOD);
         }
