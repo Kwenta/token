b9c92b530d90fca31af6b85e77853c8c100ca54a
tommyrharper <thomasrobertharper@gmail.com>
2023-05-26 14:57:47 +0100

    ✨ Fix Migrate.s.sol imports

--- scripts/Migrate.s.sol
+++ scripts/Migrate.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.19;
 
-import "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {Kwenta} from "../contracts/Kwenta.sol";
 import {StakingRewards} from "../contracts/StakingRewards.sol";
 import {SupplySchedule} from "../contracts/SupplySchedule.sol";
