c00eafd1e42a100fb32cedd0cf04fe75ebbdd74a
tommyrharper <thomasrobertharper@gmail.com>
2023-05-29 14:21:06 +0100

    ✅ Fuzz test compound function

--- test/foundry/unit/StakingRewardsV2/StakingRewardsV2Compound.t.sol
+++ test/foundry/unit/StakingRewardsV2/StakingRewardsV2Compound.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import {console} from "forge-std/Test.sol";
 import {DefaultStakingV2Setup} from "../../utils/setup/DefaultStakingV2Setup.t.sol";
 import {IStakingRewardsV2} from "../../../../contracts/interfaces/IStakingRewardsV2.sol";
 import "../../utils/Constants.t.sol";
@@ -12,15 +13,13 @@ contract StakingRewardsV2CompoundTests is DefaultStakingV2Setup {
 
     function test_compound() public {
         fundAndApproveAccountV2(address(this), TEST_VALUE);
-
         uint256 initialEscrowBalance = rewardEscrowV2.totalEscrowedBalanceOf(address(this));
 
         // stake
         stakingRewardsV2.stake(TEST_VALUE);
 
         // configure reward rate
-        vm.prank(address(supplySchedule));
-        stakingRewardsV2.notifyRewardAmount(TEST_VALUE);
+        addNewRewardsToStakingRewardsV2(TEST_VALUE);
 
         // fast forward 2 weeks
         vm.warp(2 weeks);
@@ -36,6 +35,38 @@ contract StakingRewardsV2CompoundTests is DefaultStakingV2Setup {
         uint256 totalRewards = finalEscrowBalance - initialEscrowBalance;
         assertEq(totalRewards, stakingRewardsV2.escrowedBalanceOf(address(this)));
         assertEq(totalRewards + TEST_VALUE, stakingRewardsV2.balanceOf(address(this)));
+        assertEq(rewardEscrowV2.unstakedEscrowedBalanceOf(address(this)), 0);
+    }
+
+    function test_compound_Fuzz(uint32 initialStake, uint32 newRewards) public {
+        vm.assume(initialStake > 0);
+        // need reward to be greater than duration so that reward rate is above 0
+        vm.assume(newRewards > stakingRewardsV2.rewardsDuration());
+
+        fundAndApproveAccountV2(address(this), initialStake);
+
+        uint256 initialEscrowBalance = rewardEscrowV2.totalEscrowedBalanceOf(address(this));
+
+        // stake
+        stakingRewardsV2.stake(initialStake);
+
+        // configure reward rate
+        addNewRewardsToStakingRewardsV2(newRewards);
+
+        // fast forward 2 weeks
+        vm.warp(2 weeks);
+
+        // compound rewards
+        stakingRewardsV2.compound();
+
+        // check reward escrow balance increased
+        uint256 finalEscrowBalance = rewardEscrowV2.totalEscrowedBalanceOf(address(this));
+        assertGt(finalEscrowBalance, initialEscrowBalance);
+
+        // check all escrowed rewards were staked
+        uint256 totalRewards = finalEscrowBalance - initialEscrowBalance;
+        assertEq(totalRewards, stakingRewardsV2.escrowedBalanceOf(address(this)));
+        assertEq(totalRewards + initialStake, stakingRewardsV2.balanceOf(address(this)));
         assertEq(rewardEscrowV2.unstakedEscrowedBalanceOf(address(this)), 0);
     }
 
