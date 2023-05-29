383c21d4584560fea27d1dfac7afd7218a2bcf2a
tommyrharper <thomasrobertharper@gmail.com>
2023-05-29 21:11:19 +0100

    ✅ 🪲 Fix balances checkpoints are updated fuzz

--- test/foundry/unit/StakingRewardsV2/StakingV2Checkpointing.t.sol
+++ test/foundry/unit/StakingRewardsV2/StakingV2Checkpointing.t.sol
@@ -72,11 +72,7 @@ contract StakingV2CheckpointingTests is DefaultStakingV2Setup {
         assertEq(value, 0);
     }
 
-    // function test_Balances_Checkpoints_Are_Updated_Fuzz(uint32 maxAmountStaked, uint8 numberOfRounds) public {
-    function test_Balances_Checkpoints_Are_Updated_Fuzz() public {
-        uint32 maxAmountStaked = 2;
-        uint8 numberOfRounds = 2;
-
+    function test_Balances_Checkpoints_Are_Updated_Fuzz(uint32 maxAmountStaked, uint8 numberOfRounds) public {
         vm.assume(maxAmountStaked > 0);
         // keep the number of rounds low to keep tests fast
         vm.assume(numberOfRounds < 50);
