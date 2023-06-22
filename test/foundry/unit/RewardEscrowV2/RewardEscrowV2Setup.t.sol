fb5508c2eceb6e6b26e4d63c3287e57a4faab17d
tommyrharper <thomasrobertharper@gmail.com>
2023-06-22 16:53:11 +0100

    ✅ 🧹 Use StakingV1Setup in RewardEscrowV2SetupTests

--- test/foundry/unit/RewardEscrowV2/RewardEscrowV2Setup.t.sol
+++ test/foundry/unit/RewardEscrowV2/RewardEscrowV2Setup.t.sol
@@ -2,61 +2,58 @@
 pragma solidity ^0.8.13;
 
 import {console} from "forge-std/Test.sol";
-import {Migrate} from "../../../../scripts/Migrate.s.sol";
-import {TestHelpers} from "../../utils/helpers/TestHelpers.t.sol";
-import {Kwenta} from "../../../../contracts/Kwenta.sol";
-import {RewardEscrow} from "../../../../contracts/RewardEscrow.sol";
+import {StakingV1Setup} from "../../utils/setup/StakingV1Setup.t.sol";
 import {RewardEscrowV2} from "../../../../contracts/RewardEscrowV2.sol";
-import {SupplySchedule} from "../../../../contracts/SupplySchedule.sol";
-import {StakingRewards} from "../../../../contracts/StakingRewards.sol";
 import {StakingRewardsV2} from "../../../../contracts/StakingRewardsV2.sol";
-import {MultipleMerkleDistributor} from "../../../../contracts/MultipleMerkleDistributor.sol";
 import {IRewardEscrowV2} from "../../../../contracts/interfaces/IRewardEscrowV2.sol";
-import {IERC20} from "../../../../contracts/interfaces/IERC20.sol";
 import "../../utils/Constants.t.sol";
 
 // Upgradeability imports
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract RewardEscrowV2SetupTests is TestHelpers {
+contract RewardEscrowV2SetupTests is StakingV1Setup {
     /*//////////////////////////////////////////////////////////////
-                                Setup
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+
+    address public rewardEscrowV2Implementation;
+    address public stakingRewardsV2Implementation;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        rewardEscrowV2Implementation = address(new RewardEscrowV2());
+        stakingRewardsV2Implementation = address(new StakingRewardsV2());
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                       REWARDESCROWV2 SETUP TESTS
     //////////////////////////////////////////////////////////////*/
 
     function test_Cannot_Setup_With_Owner_Zero_Address() public {
-        address treasury = createUser();
-        Kwenta kwenta = new Kwenta(
-                "Kwenta",
-                "KWENTA",
-                INITIAL_SUPPLY,
-                address(this),
-                treasury
-            );
-
-        // Deploy RewardEscrowV2
-        address rewardEscrowV2Implementation = address(new RewardEscrowV2());
         vm.expectRevert("Ownable: new owner is the zero address");
-        new ERC1967Proxy(
-                rewardEscrowV2Implementation,
-                abi.encodeWithSignature(
-                "initialize(address,address)",
-                address(0),
-                address(kwenta)
-                )
-                );
+        deployRewardEscrowV2(address(0), address(kwenta));
     }
 
     function test_Cannot_Setup_With_Kwenta_Zero_Address() public {
-        // Deploy RewardEscrowV2
-        address rewardEscrowV2Implementation = address(new RewardEscrowV2());
         vm.expectRevert(IRewardEscrowV2.ZeroAddress.selector);
-        new ERC1967Proxy(
+        deployRewardEscrowV2(address(this), address(0));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function deployRewardEscrowV2(address _owner, address _kwenta) internal returns (address) {
+        return address(
+            new ERC1967Proxy(
                 rewardEscrowV2Implementation,
                 abi.encodeWithSignature(
                 "initialize(address,address)",
-                address(this),
-                address(0)
+                _owner,
+                _kwenta
                 )
-                );
+                )
+        );
     }
 }
