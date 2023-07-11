781391a602849571856f524e8fa050771dce696e
tommyrharper <thomasrobertharper@gmail.com>
2023-07-11 13:35:03 +0100

    👷🏻 Fix pragma version to 0.8.19

--- scripts/Migrate.s.sol
+++ scripts/Migrate.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Kwenta} from "../contracts/Kwenta.sol";
--- test/foundry/utils/helpers/StakingTestHelpers.t.sol
+++ test/foundry/utils/helpers/StakingTestHelpers.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 import {console} from "forge-std/Test.sol";
 import {StakingV2Setup} from "../../utils/setup/StakingV2Setup.t.sol";
