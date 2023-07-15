8066c7d51d7ee71e5f1e5bf0844a5d511aad6259
tommyrharper <thomasrobertharper@gmail.com>
2023-07-15 17:33:12 +0100

    🚀 Update calculate-v2-balances script

--- scripts/v2-migration/calculate-v2-balances.ts
+++ scripts/v2-migration/calculate-v2-balances.ts
@@ -1,12 +1,21 @@
 import { ethers } from "hardhat";
 const data = require("./migration-transactions.json");
 
+const totalSupply = 3519169475638094792106; // 3_519 KWENTA
+const totalEscrowedBalance = 797126868540386926; // 0.7971 KWENTA
+const totalKwentaInStakingRewardsV2 = 7808422413473944539139; // 7808 KWENTA
+const totalStakedEscrow = 455395557014486600 // 0.4553 KWENTA
+const mintedToStakingRewardsV2 = 4290530229572642102152 // 4290 KWENTA
+const liquidStakedKwenta = 3518714080081080305506 // 3518 KWENTA
+
 async function main() {
+    console.log("💻 Running calculations on local data...");
+    const addressesWhoGotRewards: string[] = printInitialData();
+    console.log("🛜 Running calculations against remote data...");
+
     const [account] = await ethers.getSigners();
     console.log("Using the account:", account.address);
     console.log("Account balance:", (await account.getBalance()).toString());
-
-    printInitialData();
 
     const StakingRewardsV2 = await ethers.getContractFactory(
         "StakingRewardsV2"
@@ -15,18 +24,24 @@ async function main() {
         "0xe5bB889B1f0B6B4B7384Bd19cbb37adBDDa941a6" // The deployed contract address
     );
 
-    const totalSupply = await (await stakingRewardsV2.totalSupply()).toString();
-    console.log("totalSupply: ", totalSupply);
+    let totalStakedEscrow = 0;
 
-    const escrowedBalanceOf = await (
-        await stakingRewardsV2.escrowedBalanceOf(
-            "0x976fdc5dfa145e3cbc690e9fef4a408642732952"
-        )
-    ).toString();
-    console.log("escrowedBalanceOf :", escrowedBalanceOf);
+    for (const claimer of addressesWhoGotRewards) {
+        const escrowedBalanceOf = await (
+            await stakingRewardsV2.escrowedBalanceOf(claimer)
+        ).toString();
+        totalStakedEscrow += Number(escrowedBalanceOf);
+    }
+
+    console.log("Total V2 Staked Escrow: ", totalStakedEscrow);
+    console.log("Total KWENTA in StakingRewardsV2: 7_808 KWENTA");
+    console.log("Total KWENTA liquid staked in  StakingRewardsV2: 3_518 KWENTA");
+    console.log("Total KWENTA minted to StakingRewardsV2: 4_290 KWENTA");
+    console.log("Total KWENTA in RewardEscrowV2: 0.7971 KWENTA");
+    console.log("Total KWENTA escrow staked in RewardEscrowV2 0.4553 KWENTA");
 }
 
-function printInitialData() {
+function printInitialData(): string[] {
     const addressesWhoStaked: { [key: string]: Boolean } = {};
     const addressesWhoClaimed: { [key: string]: Boolean } = {};
     const addressesWhoCompounded: { [key: string]: Boolean } = {};
@@ -88,6 +103,8 @@ function printInitialData() {
     //     "Total stakers who did not get rewards: ",
     //     addressesWhoStakedButDidNotGetRewards
     // );
+
+    return Object.keys(addressesWhoGotRewards);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
