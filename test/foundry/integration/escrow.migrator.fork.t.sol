13a3c47b3cc1e4068cebca84b390456463055e8c
tommyrharper <thomasrobertharper@gmail.com>
2023-07-28 10:42:10 +0100

    ✅ Test RNVC

--- test/foundry/integration/escrow.migrator.fork.t.sol
+++ test/foundry/integration/escrow.migrator.fork.t.sol
@@ -955,12 +955,22 @@ contract StakingV2MigrationForkTests is EscrowMigratorTestHelpers {
         checkStateAfterStepTwo(user1, 0, 0, false);
     }
 
+    function test_RNVC() public {
+        // R
+        claimAndRegisterEntries(user1, 0, 6);
+        // N
+        createRewardEscrowEntryV1(user1, 1 ether);
+        // V
+        vest(user1, 0, 3);
+        // C
+        confirm(user1, 0, 6);
+
+        checkStateAfterStepTwo(user1, 0, 3, false);
+    }
+
 }
 
 // Up to step 2
-// RNC
-
-// RNVC
 // RVNC
 
 // RNVRVC
