e71d8b9bc6b06ac3755e622afa2724b93c7345dd
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-08-31 16:19:14 -0400

    ✅ setup test

---
+++ test/foundry/unit/NotifiableRewardAccumulator/NotifiableRewardAccumulator.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+import {Kwenta} from "../../../../contracts/Kwenta.sol";
+import {RewardEscrowV2} from "../../../../contracts/RewardEscrowV2.sol";
+import {StakingRewardsV2} from "../../../../contracts/StakingRewardsV2.sol";
+import {DefaultStakingV2Setup} from "../../utils/setup/DefaultStakingV2Setup.t.sol";
+//import {NotifiableRewardAccumulator} from "../../../../contracts/NotifiableRewardAccumulator.sol";
+
+contract NotifiableRewardAccumulatorTest is DefaultStakingV2Setup {
+
+    function setUp() public override {
+        super.setUp();
+        vm.prank(treasury);
+        kwenta.transfer(address(this), 100_000 ether);
+    }
+
+}
