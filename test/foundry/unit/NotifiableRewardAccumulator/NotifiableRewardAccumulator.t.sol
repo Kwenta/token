d9bea0775e8971c144c43a13aee4b98efe0fea32
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-09-01 12:35:07 -0400

    ✅ add StakingV2Setup

--- test/foundry/unit/NotifiableRewardAccumulator/NotifiableRewardAccumulator.t.sol
+++ test/foundry/unit/NotifiableRewardAccumulator/NotifiableRewardAccumulator.t.sol
@@ -1,28 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.19;
 
+import {console} from "../../../../lib/forge-std/src/console.sol";
 import {Kwenta} from "../../../../contracts/Kwenta.sol";
 import {RewardEscrowV2} from "../../../../contracts/RewardEscrowV2.sol";
 import {StakingRewardsV2} from "../../../../contracts/StakingRewardsV2.sol";
 import {DefaultStakingV2Setup} from "../../utils/setup/DefaultStakingV2Setup.t.sol";
 import {NotifiableRewardAccumulator} from "../../../../contracts/NotifiableRewardAccumulator.sol";
+import {StakingV2Setup} from "./StakingV2SetupWithAccumulator.t.sol";
 
-contract NotifiableRewardAccumulatorTest is DefaultStakingV2Setup {
-    NotifiableRewardAccumulator public notifiableRewardAccumulator;
+contract NotifiableRewardAccumulatorTest is StakingV2Setup {
 
     function setUp() public override {
         super.setUp();
-        notifiableRewardAccumulator = new NotifiableRewardAccumulator(
-            address(kwenta),
-            address(stakingRewardsV2),
-            address(supplySchedule)
-        );
+        notifiableRewardAccumulator.setStakingRewardsV2(address(stakingRewardsV2));
         vm.prank(treasury);
         kwenta.transfer(address(this), 100_000 ether);
         supplySchedule.setStakingRewards(address(notifiableRewardAccumulator));
     }
 
-    function testExample() public {
-        // do something
+    function testNotifiableRewardAccumulatorReceivesFunds() public {
+        vm.warp(block.timestamp + 2 weeks);
+        supplySchedule.mint();
     }
 }
