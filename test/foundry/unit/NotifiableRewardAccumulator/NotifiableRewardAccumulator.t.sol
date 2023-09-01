e87b8c3eb687e52c8b043e51325cba76e15aa91a
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-09-01 10:36:54 -0400

    👷 fix file name

--- test/foundry/unit/NotifiableRewardAccumulator/NotifiableRewardAccumulator.t.sol
+++ test/foundry/unit/NotifiableRewardAccumulator/NotifiableRewardAccumulator.t.sol
@@ -5,14 +5,20 @@ import {Kwenta} from "../../../../contracts/Kwenta.sol";
 import {RewardEscrowV2} from "../../../../contracts/RewardEscrowV2.sol";
 import {StakingRewardsV2} from "../../../../contracts/StakingRewardsV2.sol";
 import {DefaultStakingV2Setup} from "../../utils/setup/DefaultStakingV2Setup.t.sol";
-//import {NotifiableRewardAccumulator} from "../../../../contracts/NotifiableRewardAccumulator.sol";
+import {NotifiableRewardAccumulator} from "../../../../contracts/NotifiableRewardAccumulator.sol";
 
 contract NotifiableRewardAccumulatorTest is DefaultStakingV2Setup {
+
+    NotifiableRewardAccumulator public notifiableRewardAccumulator;
 
     function setUp() public override {
         super.setUp();
         vm.prank(treasury);
         kwenta.transfer(address(this), 100_000 ether);
+    }
+
+    function testExample() public {
+        // do something
     }
 
 }
