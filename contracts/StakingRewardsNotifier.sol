64ef18f60bccbe964f250acdde45fa9a3829e807
tommyrharper <thomasrobertharper@gmail.com>
2023-09-06 10:56:39 +0100

    📚 Update notifyRewardAmount delete docs for clarity

--- contracts/StakingRewardsNotifier.sol
+++ contracts/StakingRewardsNotifier.sol
@@ -76,7 +76,7 @@ contract StakingRewardsNotifier is Ownable2Step, IStakingRewardsNotifier {
 
     /// @inheritdoc IStakingRewardsNotifier
     function notifyRewardAmount(uint256 mintedAmount) external override onlySupplySchedule {
-        /// @dev delete because it is not used but cannot be removed from the function signature
+        /// @dev delete mintedAmount because it is not used but cannot be removed from the function signature
         /// as it is called by SupplySchedule which is immutable and expects to pass this value
         /// instead currentBalance is used
         delete mintedAmount;
