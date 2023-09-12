29e284103ffcc1dc3f3e5b0dacb3bbaf65ab6318
tommyrharper <thomasrobertharper@gmail.com>
2023-09-12 13:53:57 +0100

    ✅ 🔧 Fix recoverExcessFunds invariant

--- test/foundry/integration/escrow.migrator.invariant.t.sol
+++ test/foundry/integration/escrow.migrator.invariant.t.sol
@@ -90,6 +90,7 @@ contract EscrowMigratorInvariantTests is StakingV2Setup {
     }
 
     function invariant_Recover_Always_Any_Excess_Funds() public {
+        vm.prank(owner);
         handler.recoverExcessFunds();
         assertLe(
             kwenta.balanceOf(address(escrowMigrator)),
